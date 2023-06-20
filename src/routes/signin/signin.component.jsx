d16e49eff30629733c18ec199cf450dca8fc96c0
Rehan-Imran <rehanimranam@gmail.com>
2023-06-21 02:35:42 +0500

    google redirect login flow implemented but commented because not required

--- src/routes/signin/signin.component.jsx
+++ src/routes/signin/signin.component.jsx
@@ -1,17 +1,36 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 import {
+  //   auth,
   signInWithGooglePopup,
+  signinWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const Signin = () => {
+  //   const getData = async () => {
+  //     const response = await getRedirectResult(auth);
+  //     if (response) {
+  //       const userDocRef = await createUserDocumentFromAuth(response.user);
+  //     }
+  //     console.log(response);
+  //   };
+  //   useEffect(() => {
+  //     getData();
+  //   }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign in</h1>
       <button onClick={logGoogleUser}>Sign in with Google</button>
+      {/* <button onClick={signinWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
     </div>
   );
 };
