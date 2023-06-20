7ffdeaea7e366e3c46cfcafbe6e052f72718d24e
Rehan-Imran <rehanimranam@gmail.com>
2023-06-21 02:08:59 +0500

    added sign in functionctionality with firebase gooble authentication and using firebase utilities

---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,62 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  Firestore,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBk6z6qgWmcbsEDeVlSz3hE9ZzFzLdgqlw",
+  authDomain: "crwn-cloathing-db-ztm.firebaseapp.com",
+  projectId: "crwn-cloathing-db-ztm",
+  storageBucket: "crwn-cloathing-db-ztm.appspot.com",
+  messagingSenderId: "714600381117",
+  appId: "1:714600381117:web:e0b11e0457dfbe3cad95d4",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return userDocRef;
+};
